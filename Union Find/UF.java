c1f7c428b8a6844bc043dad093d4c45ab06f3e0f
Dave Fife <davidmfife@outlook.com>
2022-10-17 19:24:48 -0400

    Add files via upload

--- Union Find/UF.java
+++ Union Find/UF.java
@@ -5,6 +5,8 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+
+
 public class UF {
     private int[] id;   //access to component id (site indexed)
     private int count;  //number of components
@@ -22,26 +24,35 @@ public class UF {
     public boolean connected(int p, int q){
         return find(p) == find(q);}
     
-    public int find(int p);// need to implement find and union
-    public void union(int p, int q)
+    public int find(int p){ 
+        return id[p];
+    }
+    public void union(int p, int q){
+        int pID = find(p);
+        int qID = find(q);
+        
+        if(pID == qID) return;
+        
+        for(int i = 0; i < id.length; i++)
+            if (id[i] == pID) id[i] = qID;
+        count--;    
+   
+    }
     
     public static void main(String[] args){
         int n = StdIn.readInt();  //read number of sites
         UF uf = new UF(n);
         while (!StdIn.isEmpty()){
-            int n = StdIn.readInt();
-            UF uf = new UF(n);
-            while (!StdIn.isEmpty()){
                 int p = StdIn.readInt();
                 int q = StdIn.readInt();
                 if (uf.connected(p, q)) continue;
                 uf.union(p, q);
                 StdOut.println(uf.count() + " Components");
             
-            }
+        }
         
         
         }
     }
 
-}
+
