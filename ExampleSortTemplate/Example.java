d7ac41c37e92872cb86a5b9a4264ebad4e21c559
Dave Fife <davidmfife@outlook.com>
2022-10-25 19:31:02 -0400

    Add files via upload

--- ExampleSortTemplate/Example.java
+++ ExampleSortTemplate/Example.java
@@ -7,7 +7,38 @@
  */
 public class Example {
     public static void sort(Comparable[] a)
-    {/* see different sort algorithms */}
+    {/* see different sort algorithms 
+        Selection sort:  find smallest element in array exchange with first position;
+                         find second smallest exchange with second position;
+                         continue until sorted.
+        Drawback: Running-time is insensitive to input, must search each place.
+        Benefit:  Data Movement is minimal( linear  using n exchanges).
+        
+       
+        
+        int n = a.length;
+        for (int i= 0; i<n; i++){
+            int min = i;
+            for(int j=i+1; j<n; j++)
+                if (less(a[j], a[min])) min = j;
+            exchange(a, i, min);
+        
+        }
+        
+        insertion sort
+        works by shuffling in the right order like organizing a hand of cards.
+        best if the array is nearly already sorted.
+        on average uses about half the compares of seletion sort.
+        
+        
+        int n = a.length;
+        for ( int i =1; i< n; i++){
+            for (int j = i; j>0 && less(a[j], a[j-1]); j--)
+                exchange(a, j, j-1);
+        
+        }
+        */
+    }
     
     private static boolean less(Comparable v, Comparable w)
     { return v.compareTo(w) < 0;}
